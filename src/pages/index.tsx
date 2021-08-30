8378424bd2631aa7a4740ba446ac66a39e3fe61a
Victor Azeredo Trindade <victor@MacBook-Air-de-Victor.local>
2021-08-30 13:38:20 -0300

    First commit

---
+++ src/pages/index.tsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import type { NextPage } from 'next'
+import Head from 'next/head'
+
+const Home: NextPage = () => {
+  return (
+    <div>
+      <Head>
+        <title>Create Next App</title>
+      </Head>
+
+      <main>
+        <h1>Hellooooo</h1>
+      </main>
+    </div>
+  )
+}
+
+export default Home
